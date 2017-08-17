380c4badcefc52c1c46e2b3ba130d50b36e8c2c5
Aleksandr Kojemyako <alekskojem@gmail.com>
2017-08-17 11:29:52 +0300

    Gett params from POST query

--- app.rb
+++ app.rb
@@ -14,3 +14,13 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	
+	erb "OK, Имя посетителя #{@username}, контактный телфон #{@phone}, указано время #{@datetime}, имя мастера #{@barber}."
+end
